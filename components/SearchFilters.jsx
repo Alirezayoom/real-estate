decb31c983416c01d458e86c2adaba20cded6c76
alireza <alirezayoom@gmail.com>
2022-10-27 22:48:18 +0430

    search filter component added

---
+++ components/SearchFilters.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+import {
+  Flex,
+  Box,
+  Select,
+  Text,
+  Input,
+  Spinner,
+  Icon,
+  Button,
+} from "@chakra-ui/react";
+
+const SearchFilters = () => {
+  return <div>searchfilterz</div>;
+};
+
+export default SearchFilters;
